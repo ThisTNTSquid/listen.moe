d844025a22ab81041960b00c33ba90abd1967ad9
ThisTNTSquid <wonghoyeung1109@gmail.com>
2018-12-11 15:38:51 +0900

    Repo init

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+// const DiscordJS = require("discord.js");
+const config = require("config");
+// const sqlite = require("sqlite");
+const { AkairoClient } = require("discord-akairo");
+
+class Bot extends AkairoClient {
+  constructor() {
+    super(
+      {
+        ownerID: config.get("owners"),
+        prefix: config.get("prefix"),
+        handleEdits: true,
+        commandUtil: true,
+        allowMention: true,
+        commandDirectory: "./commands/",
+        inhibitorDirectory: "./inhibitors/",
+        listenerDirectory: "./listeners/",
+        emitters: {
+          process
+        }
+      },
+      { disableEveryone: true }
+    );
+
+  }
+}
+
+const bot = new Bot();
+bot.build();
+bot.login(process.env.TOKEN);
